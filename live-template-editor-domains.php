262caea6ea4eb3a29d9accc1bea09d1c30397429
Raphaël Dartigues <raphael.dartigues@gmail.com>
2020-12-13 18:42:06 +0545

    Update live-template-editor-domains.php

--- live-template-editor-domains.php
+++ live-template-editor-domains.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Live Template Editor Domains
- * Version: 1.0.2.4
+ * Version: 1.0.2.5
  * Plugin URI: https://github.com/rafasashi
  * Description: Domain and subdomain management addon for Live Template Editor
  * Author: Rafasashi
