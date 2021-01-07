ab137cefd11bfa8874f836cfb1b771d6ef840b00
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2021-01-07 11:12:03 +0545

    profile menu api implemented

--- views/profile.php
+++ views/profile.php
@@ -4,8 +4,6 @@ $ltple = LTPLE_Client::instance();
 
 include_once( $ltple->views . '/profile/header.php' );
 
-include_once( $ltple->views . '/profile/navbar.php' );
-
 ?>
 		
 	<main id="main" class="site-main ltple-domains" role="main">
